b365e547640c6306681b356b0b5d962b84717ce3
mesikhicher49-cmd <mesikhicher49@gmail.com>
2026-03-27 20:36:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,28 @@
+export default async function handler(req, res) {
+  try {
+    const { q } = req.query;
 
+    if (!q) {
+      return res.status(400).json({ error: "Missing user_id (q)" });
+    }
+
+    // Original API call
+    const response = await fetch(`https://tg-to-num-six.vercel.app/?key=rootxsuryansh&q=${q}`);
+    const data = await response.json();
+
+    // 🔥 Modify response
+    delete data.credit;
+    delete data.owner;
+    delete data.admin;
+
+    // 👇 TERA CUSTOM
+    data.owner = "@ZyroX9";
+    data.credit = "@ZyroXZone";
+    data.note = "Api BY Zyro 🚀";
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+}
